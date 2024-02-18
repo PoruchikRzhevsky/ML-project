cb1af887c2dc09dc1892ec6b92f5cbe88eeb31e3
Artem Gorodilov <sheportplay@gmail.com>
2024-02-18 14:18:36 -0800

    Commenting the code, changing visualisation, optimazing the code and changing game and nn parametrs for best performance

--- visualize.py
+++ visualize.py
@@ -1,9 +1,12 @@
+#https://github.com/CodeReclaimers/neat-python/blob/master/examples/xor/visualize.py
+
 import warnings
 
 import graphviz
 import matplotlib.pyplot as plt
 import numpy as np
 
+from IPython.display import display
 
 def plot_stats(statistics, ylog=False, view=False, filename='avg_fitness.svg'):
     """ Plots the population's average and best fitness. """
@@ -176,6 +179,8 @@ def draw_net(config, genome, view=False, filename=None, node_names=None, show_di
             width = str(0.1 + abs(cg.weight / 5.0))
             dot.edge(a, b, _attributes={'style': style, 'color': color, 'penwidth': width})
 
-    dot.render(filename, view=view)
+    # dot.render(filename, view=view)
+    if view:
+        display(dot)
 
     return dot
